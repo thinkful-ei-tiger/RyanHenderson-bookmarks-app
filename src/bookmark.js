2c2915b44bad936fa162404efc85797608aadfe4
RyanHenderson9001 <70710566+RyanHenderson9001@users.noreply.github.com>
2020-10-30 20:50:18 -0400

    3rd try

--- src/bookmark.js
+++ src/bookmark.js
@@ -71,7 +71,7 @@ const mainPage = function(){
       <input type="button" value="add bookmark" class='AddBookmark'>
   <div>
       <label for="filter">Filter by:</label>
-      <select id="filter" name="filterlist" form="form">
+      <select id="filter" name="filterlist" form="filter">
       <option value="1">1</option>
       <option value="2">2</option>
       <option value="3">3</option>
